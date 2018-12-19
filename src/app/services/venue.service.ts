673db01ddd47bca5769f01fc8aba272580c21cab
Matuki92 <matiaspierretti2@hotmail.com>
2018-12-19 20:35:30 +0100

    added venue auth service

--- src/app/services/venue.service.ts
+++ src/app/services/venue.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import 'rxjs/add/operator/toPromise';
 
-@Injectable({
-  providedIn: 'root'
-})
+// import { environment } from '../../environments/environment';
+
+@Injectable()
 export class VenueService {
 
-  constructor() { }
+  private user: any;
+  private userChange: Subject<any> = new Subject();
+
+  private API_URL = 'http://localhost:3000/venues';
+
+  userChange$: Observable<any> = this.userChange.asObservable();
+
+  constructor(private httpClient: HttpClient) { }
+
+  private setUser(user?: any) {
+    this.user = user;
+    this.userChange.next(user);
+    return user;
+  }
+
+  me(): Promise<any> {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.get(`${this.API_URL}/me`, options)
+      .toPromise()
+      .then((user) => this.setUser(user))
+      .catch((err) => {
+        if (err.status === 404) {
+          this.setUser();
+        }
+      });
+  }
+
+  login(user: any): Promise<any> {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.post(`${this.API_URL}/login`, user, options)
+      .toPromise()
+      .then((data) => this.setUser(data));
+  }
+
+  logout(): Promise<any> {
+    const options = {
+      withCredentials: true
+    };
+    return this.httpClient.post(`${this.API_URL}/logout`, {}, options)
+      .toPromise()
+      .then(() => this.setUser());
+  }
+
+  getUser(): any {
+    return this.user;
+  }
 }
