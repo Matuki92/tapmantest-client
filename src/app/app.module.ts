94b760b7ead9aae3fedd94077502b19510cd0b1e
Matuki92 <matiaspierretti2@hotmail.com>
2018-12-19 11:00:58 +0100

    initial setup, added routes module and homepage/admin components. test ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// components
 import { AppComponent } from './app.component';
+import { BeerCardComponent } from './components/beer-card/beer-card.component';
+
+//pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+
+const routes: Routes = [
+   { path: '',  component: HomePageComponent },
+   { path: 'admin', component: AdminPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AdminPageComponent,
+    BeerCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
